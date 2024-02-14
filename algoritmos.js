d9118471a84120057c8af02a15d7cfe4ae636dec
jorgearma <jorgearma@thaper.ROOMS-DK>
2024-02-14 23:25:48 +0000

    14-2

--- algoritmos.js
+++ algoritmos.js
@@ -1,72 +1,25 @@
-// ordenamiento con merge
-const margesort = (arr) => {
-    if (arr.length <= 1) {
-        return arr
-    }
-    let medio = Math.floor(arr.length / 2)
-    let left = arr.slice(0 ,medio)
-    let rigth = arr.slice(medio)
 
-    return merge(margesort(left), margesort(rigth))
+const numerosAleatorios = []
+
+for (let i = 0; i < 10; i++) {
+    const Alatorio = Math.floor(Math.random()*10)
+    numerosAleatorios.push(Alatorio)
 }
 
-const  merge = ( left, rigth) => {
-    let indexL = 0
-    let indexD = 0
-    let result = []
-    while (indexL < left.length && indexD < rigth.length) {
-        if (left[indexL] <  rigth[indexD]) {
-            result.push(left[indexL])
-            indexL++
-        } else {
-            result.push(rigth[indexD])
-            indexD++
-        }
-    }
-    return [...result, ...left.slice(indexL), ...rigth.slice(indexD)]
-}
-
-//generar array de nuemors aliatorios
-var  numeros = []
-    for (let i = 0; i < 10 ; i++) {
-        var numerosaletorios = Math.floor(Math.random()*10)
-        numeros.push(numerosaletorios)         
-    }    
- console.log(numeros);
-
- console.log(result);
+console.log(numerosAleatorios);
 
 
- //busqueda binaria 
-
- const busquedaBinaria = (busqeuda,arr)=> {
-        let inicio = 0
-        let fin = arr.length
-        while (inicio < fin) {
-            let medio = Math.floor((inicio + fin) / 2)
-            if (arr[medio] === busqeuda) {
-                return medio
-            }else if (arr[medio] < busqeuda) {
-                inicio = medio + 1
-            } else {
-                fin = medio - 1
-            }
-        }
-        return -1
- }
-
-const numeroabuscar = 7
-const buscado = busquedaBinaria(numeroabuscar,result)
-console.log(buscado);
-
-//insertsort
-
-const insert = ( arr )=>{
-    for (let i = 0; i < arr.length; i++) {
+const inserSort = (array)=>{
+    for (let i = 1; i < array.length; i++) {
         const element = array[i];
-        const j = i - 1
-        while (j >= 0 && arr[j] > element) {
-            
+        let j = i - 1
+        while (j >= 0 && array[j] > element) {
+            array[j + 1] = array[j ]
+            j--
         }
+        array[j + 1] = element
     }
+    return array
 }
+const numerosOrdenados = inserSort(numerosAleatorios)
+console.log(numerosOrdenados);
--- tareas.js
+++ tareas.js
@@ -1,15 +1,9 @@
-const aletorios = []
-    for (let i = 0; i < 10; i++) {
-        let resultaletorio = Math.floor(Math.random()*10)
-        aletorios.push(resultaletorio)        
-    }
-console.log(aletorios);
-
-const insertsort = (array) => {
+const insertSort = (array) =>{
     for (let i = 1; i < array.length; i++) {
         const element = array[i];
         let j = i - 1
-        while (j >= 0 && element < array[j]) {
+
+        while (j >= 0 && array[j] > element) {
             array[j + 1] = array[j]
             j--
         }
@@ -18,124 +12,86 @@ const insertsort = (array) => {
     return array
 }
 
-const resultinsert = insertsort(aletorios)  
-console.log('result inset' , resultinsert);
-
-
-const quicksort = (array) =>{
-    if (array.length <= 1) {
-        return array
-    }
-    let izquierda = []
-    let derecha = []
-    let pivote = array[0]
-
-    for (let i = 1; i < array.length; i++) {
-        if (array[i] < pivote) {
-            izquierda.push(array[i])
-        } else {
-            derecha.push(array[i])
-        }        
-    }
-    return [...quicksort(izquierda) , pivote , ...quicksort(derecha)]
+const aleatorios = []
+for (let i  = 0; i < 10; i++) {
+    const creador = Math.floor(Math.random()*10)
+    aleatorios.push(creador)
 }
+console.log(aleatorios);
 
-const resultquick = quicksort(aletorios)
-console.log('result quick ' , resultquick);
+const resultInsert = insertSort(aleatorios)
+console.log(resultInsert);
 
-const mergesort = (array)=>{
-    
-    if (array.length <= 1) {
-        return array
-    }
-    let medio = Math.floor(array.length / 2)
-    let left = array.slice(0 , medio)
-    let right = array.slice(medio)
+const busquedaBinaria = (array, numero) =>{
+    let inicio = 0
+    let fin = array.length - 1
 
-    return merge(mergesort(left), mergesort(right))
-}
-const merge = ( left , right) =>{
-    let indexL = 0
-    let indexR = 0
-    let result = []
-    
-    while (indexL < left.length && indexR < right.length) {
-        if (left[indexL] < right[indexR]) {
-            result.push(left[indexL])
-            indexL++
-        }else{
-            result.push(right[indexR])
-            indexR++
-        }
-    }
-    return [...result, ...left.slice(indexL) , ...right.slice(indexR)]
-}
-
-const resultmerge = mergesort(aletorios)        
-console.log('result merge' , resultmerge);
-
-const busquedabinaria = (busqueda , array) =>{
-    let inicio = 0 
-    let final = array.length
-
-    while (inicio < final) {
-        let medio = Math.floor((inicio + final) / 2)
-        if (array[medio] === busqueda) {
+    while (inicio <= fin) {
+        let medio = Math.floor((inicio + fin) / 2)
+        if (array[medio] === numero) {
             return medio
-        }else if (array[medio] < busqueda) {
+        }else if (array[medio] < numero) {
             inicio = medio + 1
-        } else {
-            final = medio - 1
+        }else{
+            fin = medio -1
         }
     }
     return -1
-
 }
 
-const resultbusquedaBinaria = busquedabinaria(4, aletorios) 
-console.log(resultbusquedaBinaria);
-
-
-class node{
-    constructor(data){
-        this.data = data
-        this.next = null
-    }
+const resultbsuqeda = busquedaBinaria(aleatorios,4)
+if (resultbsuqeda === -1) {
+    console.log('no encontrado');
+}else{
+    console.log(resultbsuqeda);
 }
 
-class listaenlazada{
-    constructor(){
-        this.head = null
+
+const quickSort = (array) =>{
+    if (array.length <= 0) {
+        return array
     }
-    appent(data){
-        const newNode = new node(data);
-        if (!this.head) {
-            this.head = newNode
+    const [pivote , ...resto] = array
+    const left = []
+    const rigth = []
+
+    for (const iterator of resto) {
+        iterator < pivote  ? left.push(iterator) : rigth.push(iterator)
+    }
+
+    return [...quickSort(left) , pivote , ...quickSort(rigth)]
+}
+const resultQuicSort = quickSort(aleatorios)
+console.log(resultQuicSort);
+
+
+const margeSort = (array) =>{
+    if (array.length <= 1) {
+        return array
+    }
+    const medio = Math.floor(array.length / 2)
+    const leftt = array.slice(0,medio)
+    const rigthh = array.slice(medio)
+
+    return merge(margeSort(leftt) , margeSort(rigthh))
+}
+
+const merge = (leftt , rigthh) =>{
+    const result = []
+    let indexL = 0
+    let indexD = 0
+
+    while (indexL < leftt.length && indexD < rigthh.length) {
+        if (leftt[indexL] < rigthh[indexD]) {
+            result.push(leftt[indexL])
+            indexL ++
         }else{
-            let current = this.head
-            while (current.next) {
-                current = current.next
-            }
-            current.next = newNode
+            result.push(rigthh[indexD])
+            indexD ++
         }
     }
-    imprimir(){
-        let current = this.head
-        while (current) {
-            console.log(current.data);
-            current = current.next
-        }
-    }
+    return [...result , ...leftt.slice(indexL) , ...rigthh.slice(indexD)]
 }
 
-const lista = new listaenlazada();
-
-lista.appent(10)
-lista.appent(10)
-lista.appent(10)
-lista.appent(10)
-lista.appent(10)
-lista.appent(10)
-lista.appent(10)
-
-lista.imprimir()
+const resultmerge = margeSort(aleatorios)
+console.log(resultmerge, 'merge');
